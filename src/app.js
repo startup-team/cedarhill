c92aa81283aa176a69067908f6a68195eef2dfa3
samueltrejo <samuel.trejo@s.tcatnashville.edu>
2019-10-15 16:49:59 -0500

    converted project to react

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles/app.scss';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <div>Hello World</div>
+      </div>
+    );
+  }
+}
+
+export default App;
