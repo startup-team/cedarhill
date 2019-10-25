5c5f321072ace8a5623503517d5cfa1a95830282
Samuel T. Mendoza <samuel.mendoza@uniguest.com>
2019-10-25 14:44:15 -0500

    moved background image property to react side

--- src/components/home-hero.js
+++ src/components/home-hero.js
@@ -3,7 +3,9 @@ import React from 'react';
 class Hero extends React.Component {
   render() {
     return (
-      <div className="hero">
+      <div className="hero" style={{backgroundImage:
+        'linear-gradient(rgba(0, 0, 0, 0.5),rgba(0, 0, 0, 0.5)), url(https://codetheweb.blog/assets/img/posts/full-image-hero/image.jpg)',
+      }}>
           <div className="hero-content">
             <h1>Cedar Hill Baptist</h1>
             <h2>Welcome to Cedar Hill Baptist Church</h2>
